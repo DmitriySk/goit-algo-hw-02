9ea031faff7185187e2d0d5536ae961d6df81df6
Dmytro Sk. <dima.skalskij@gmail.com>
2026-04-23 20:01:47 +0300

    fix task 2 for ukrainian lang

--- task2.py
+++ task2.py
@@ -2,11 +2,12 @@ from collections import deque
 import re
 
 def check_palindrome_simple(str):
-    str_cleaned = re.sub(r'[^A-Za-z0-9]', '', str).lower()
+    str_cleaned = re.sub(r'[^A-Za-zА-Яа-я0-9]', '', str).lower()
     return str_cleaned == str_cleaned[::-1]
 
 def check_palindrome_deque(str):
-    str_cleaned = re.sub(r'[^A-Za-z0-9]', '', str).lower()
+    str_cleaned = re.sub(r'[^A-Za-zА-Яа-я0-9]', '', str).lower()
+    print(str_cleaned)
     str_list = deque(str_cleaned.lower())
     while len(str_list):
         if len(str_list) == 1:
@@ -20,7 +21,7 @@ str2 = 'Was it a car or a cat I saw?'
 str3 = '12321'
 str4 = 'Я несу гусеня'
 str5 = 'Хата на канатах'
-str6 = 'Аргентина манить негра'
+str6 = 'Три психи пили Пилипихи спирт'
 
 strings = [str1, str2, str3, str4, str5, str6]
 
