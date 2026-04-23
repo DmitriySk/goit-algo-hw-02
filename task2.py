7f375546b2ad542a76af90a7b7b98f0acf6916d5
Dmytro Sk. <dima.skalskij@gmail.com>
2026-04-23 19:56:44 +0300

    task 2

---
+++ task2.py
@@ -0,0 +1,30 @@
+from collections import deque
+import re
+
+def check_palindrome_simple(str):
+    str_cleaned = re.sub(r'[^A-Za-z0-9]', '', str).lower()
+    return str_cleaned == str_cleaned[::-1]
+
+def check_palindrome_deque(str):
+    str_cleaned = re.sub(r'[^A-Za-z0-9]', '', str).lower()
+    str_list = deque(str_cleaned.lower())
+    while len(str_list):
+        if len(str_list) == 1:
+            return True
+        if str_list.popleft() != str_list.pop():
+            return False
+    return True
+
+str1 = 'A man, a plan, a canal: Panama'
+str2 = 'Was it a car or a cat I saw?'
+str3 = '12321'
+str4 = 'Я несу гусеня'
+str5 = 'Хата на канатах'
+str6 = 'Аргентина манить негра'
+
+strings = [str1, str2, str3, str4, str5, str6]
+
+for str in strings:
+    print(str)
+    print(check_palindrome_simple(str))
+    print(check_palindrome_deque(str), end='\n\n')
